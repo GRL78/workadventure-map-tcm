fe422d9b35bc5d57d9cd44ce1b1381445e642480
GRL <g.lesniewski@thecodingmachine.com>
2021-07-21 10:16:49 +0200

    Fix image doesn't exist Update script to use new Script Api

--- paris/src/floor0.ts
+++ paris/src/floor0.ts
@@ -1,72 +1,72 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-var zoneWelcomeToTCMName = "popUpWelcomeToTCM";
-var zoneMeetTheDevTeamName = "popUpMeetTheDevTeam";
-var zoneRespectPeopleName = "popUpRespectPeople";
-var zoneTCMAroundTheWorld = "popUpTCMAroundTheWorld";
+const zoneWelcomeToTCMName = "popUpWelcomeToTCM";
+const zoneMeetTheDevTeamName = "popUpMeetTheDevTeam";
+const zoneRespectPeopleName = "popUpRespectPeople";
+const zoneTCMAroundTheWorld = "popUpTCMAroundTheWorld";
 
-var urlWelcomeToTCM = "https://www.thecodingmachine.com/en/welcome-to-the-coding-machine/";
-var urlTCMAroundTheWorld = "https://www.thecodingmachine.com/en/the-coding-machine-around-the-world/";
+const urlWelcomeToTCM = "https://www.thecodingmachine.com/en/welcome-to-the-coding-machine/";
+const urlTCMAroundTheWorld = "https://www.thecodingmachine.com/en/the-coding-machine-around-the-world/";
 
-var currentPopup : any = undefined;
-var isCoWebSiteOpened =  false;
+let currentPopup : any = undefined;
+let isCoWebSiteOpened =  false;
 
-WA.onEnterZone(zoneWelcomeToTCMName, () => {
-    currentPopup =  WA.openPopup("popUpWelcome","Hey ! Welcome to The Coding Machine !",[
+WA.room.onEnterZone(zoneWelcomeToTCMName, () => {
+    currentPopup =  WA.ui.openPopup("popUpWelcome","Hey ! Welcome to The Coding Machine !",[
         {
             label: "Who are we ? ",
             className: "normal",
-            callback: (popup => {
-                WA.openCoWebSite(urlWelcomeToTCM);
+            callback: (() => {
+                WA.nav.openCoWebSite(urlWelcomeToTCM);
                 isCoWebSiteOpened = true;
-                closePopUp2();
+                closePopUp();
             })
         }]);
 })
 
-WA.onEnterZone(zoneTCMAroundTheWorld, () => {
-    currentPopup =  WA.openPopup("popUpTCMAroundTheWorld","The Coding Machine around the World",[
+WA.room.onEnterZone(zoneTCMAroundTheWorld, () => {
+    currentPopup =  WA.ui.openPopup("popUpTCMAroundTheWorld","The Coding Machine around the World",[
         {
             label: "See more ",
             className: "normal",
-            callback: (popup => {
-                WA.openCoWebSite(urlTCMAroundTheWorld);
+            callback: (() => {
+                WA.nav.openCoWebSite(urlTCMAroundTheWorld);
                 isCoWebSiteOpened = true;
-                closePopUp2();
+                closePopUp();
             })
         }]);
 })
 
-WA.onEnterZone(zoneRespectPeopleName, () => {
-    currentPopup =  WA.openPopup("popUpRespect","Thank you for respecting people at work :D",[])
+WA.room.onEnterZone(zoneRespectPeopleName, () => {
+    currentPopup =  WA.ui.openPopup("popUpRespect","Thank you for respecting people at work :D",[])
 })
 
-WA.onEnterZone(zoneMeetTheDevTeamName, () => {
-    currentPopup =  WA.openPopup("popUpMeetDev","Meet the WorkAdventure dev team ! ",[]);
+WA.room.onEnterZone(zoneMeetTheDevTeamName, () => {
+    currentPopup =  WA.ui.openPopup("popUpMeetDev","Meet the WorkAdventure dev team ! ",[]);
 })
 
-WA.onLeaveZone(zoneRespectPeopleName, closePopUp2)
+WA.room.onLeaveZone(zoneRespectPeopleName, closePopUp)
 
-WA.onLeaveZone(zoneMeetTheDevTeamName, closePopUp2)
+WA.room.onLeaveZone(zoneMeetTheDevTeamName, closePopUp)
 
-WA.onLeaveZone(zoneWelcomeToTCMName, () =>{
-    closePopUp2();
+WA.room.onLeaveZone(zoneWelcomeToTCMName, () =>{
+    closePopUp();
 
     if (isCoWebSiteOpened) {
-        WA.closeCoWebSite();
+        WA.nav.closeCoWebSite();
         isCoWebSiteOpened = false;
     }
 })
 
-WA.onLeaveZone(zoneTCMAroundTheWorld, () =>{
-    closePopUp2();
+WA.room.onLeaveZone(zoneTCMAroundTheWorld, () =>{
+    closePopUp();
 
     if (isCoWebSiteOpened) {
-        WA.closeCoWebSite();
+        WA.nav.closeCoWebSite();
         isCoWebSiteOpened = false;
     }
 })
 
-function closePopUp2(){
+function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
